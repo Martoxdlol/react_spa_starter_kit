f5722d62955814498edc606b32c06d9b68595631
Tomás Cichero <martoxdlol@gmail.com>
2024-08-29 19:11:56 -0300

    install shadcn-ui

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Button } from './components/ui/button'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <div className='bg-red-500 border-4 border-dashed' >
-            hello!
+        <div className='bg-red-500 border-4 border-dashed'>
+            <Button variant="outline">Click ASD</Button>
         </div>
     </StrictMode>,
 )
