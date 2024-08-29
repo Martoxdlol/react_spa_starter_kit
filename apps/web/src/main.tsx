949d33992a1eead9f6ad5759cc58377759a13c11
Tomás Cichero <martoxdlol@gmail.com>
2024-08-29 19:03:45 -0300

    install tailwind

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <div />
+        <div className='bg-red-500 border-4 border-dashed' >
+            hello!
+        </div>
     </StrictMode>,
 )
